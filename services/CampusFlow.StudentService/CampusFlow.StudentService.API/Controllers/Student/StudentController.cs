92013241aa54dfb81416b29b8b20f02964e72094
Abhishek Upadhyaya <abhishek.upadhyaya23@gmail.com>
2026-07-04 23:05:48 +0530

    Created Get EndPoint and configure SwaggerUI
    Created Get EndPoint and configure SwaggerUI

---
+++ services/CampusFlow.StudentService/CampusFlow.StudentService.API/Controllers/Student/StudentController.cs
@@ -0,0 +1,58 @@
+using CampusFlow.StudentService.API.Contracts.Requests.Student;
+using CampusFlow.StudentService.API.Contracts.Responses.Student;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CampusFlow.StudentService.API.Controllers.Student;
+
+    [Route("api/v1/students")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<StudentListResponse> GetStudents([FromQuery] GetStudentsRequest request)
+        {
+        var response = new StudentListResponse
+        {
+            Items =
+            [
+                new StudentResponse
+                    {
+                        Id = Guid.NewGuid(),
+                        FirstName = "John",
+                        LastName = "Doe",
+                        Email = "Jhon.Doe@gmail.com",
+                        MobileNumber = "1234567890",
+                        ClassId = Guid.NewGuid(),
+                        ClassName = "Class A",
+                        Gender = "Male",
+                        DateOfBirth = new DateOnly(1990, 10, 1),
+                        Address= "123 Main St, City, Country",
+                        Status= "Active"
+                    },
+                    new StudentResponse
+                    {
+                        Id = Guid.NewGuid(),
+                        FirstName = "Abhishek",
+                        LastName = "Upadhyaya",
+                        Email = "abhishek.upadhyaya23@gmail.com",
+                        MobileNumber = "9871001204",
+                        ClassId = Guid.NewGuid(),
+                        ClassName = "Class B",
+                        Gender = "Male",
+                        DateOfBirth = new DateOnly(1990, 10, 23),
+                        Address= "123 Main St, City, Country",
+                        Status= "Active"
+                    }
+                ],
+            Page = request.Page,
+            PageSize = request.PageSize,
+            TotalRecords = 2,
+            TotalPages = 1
+        };
+        return Ok(response);
+
+    }
+        }
+
+
