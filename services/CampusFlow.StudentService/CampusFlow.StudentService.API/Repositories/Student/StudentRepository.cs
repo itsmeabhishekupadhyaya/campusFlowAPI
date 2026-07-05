48ee97c5aea507b5e04ee76bdf336ba86423356b
Abhishek Upadhyaya <abhishek.upadhyaya23@gmail.com>
2026-07-05 20:42:14 +0530

    Added GetStudent and GetStudentById Implementation
    Added GetStudent and GetStudentById Implementation

--- services/CampusFlow.StudentService/CampusFlow.StudentService.API/Repositories/Student/StudentRepository.cs
+++ services/CampusFlow.StudentService/CampusFlow.StudentService.API/Repositories/Student/StudentRepository.cs
@@ -1,4 +1,5 @@
 
+using CampusFlow.StudentService.API.Contracts.Common.Enums;
 using CampusFlow.StudentService.API.Domain.Entities;
 using CampusFlow.StudentService.API.Persistence;
 using Microsoft.EntityFrameworkCore;
@@ -17,10 +18,43 @@ namespace CampusFlow.StudentService.API.Repositories;
         return student;
     }
 
-    //public async Task<ICollection<Student>> GetStudentsAsync()
-    //{
-    //   return await _DbContext.Students.ToListAsync();    
-    //}
+    public async Task<int> CountAsync(string? search)
+    {
+        IQueryable<Student> query = _DbContext.Students.AsNoTracking();
+
+        if(!string.IsNullOrEmpty(search))
+        {
+            query = query.Where(s => s.FirstName.Contains(search) || s.LastName.Contains(search) || s.Email.Contains(search));
+        }
+        return await query.CountAsync();
+    }
+
+    public async Task<Student?> GetStudentByIdAsync(Guid id)
+    {
+        return await _DbContext.Students
+            .AsNoTracking()
+            .FirstOrDefaultAsync(s => s.Id == id);
+    }
+
+    public async Task<IReadOnlyList<Student>> GetStudentsAsync(int page, int Pagesize, string? search, string sortBy, SortDirection sortDirection)
+    {
+        IQueryable<Student> query = _DbContext.Students.AsNoTracking();
+
+        if(!string.IsNullOrEmpty(search))
+        {
+            query = query.Where(s => s.FirstName.Contains(search) || s.LastName.Contains(search) || s.Email.Contains(search));
+        }
+        query = sortDirection == SortDirection.Descending
+            ? query.OrderByDescending(student => student.FirstName)
+            : query.OrderBy(student => student.FirstName);
+
+        query = query
+            .Skip((page - 1) * Pagesize)
+            .Take(Pagesize);
+
+        return await query.ToListAsync();
+    }
+    
 
     public async Task SaveChangesAsync()
     {
